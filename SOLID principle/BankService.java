7a23497ee936dc8cfc42d7521769de0f34b73506
Rasel <103592581+alfred-rasel@users.noreply.github.com>
2023-03-31 23:52:35 +0600

    Add files via upload

---
+++ SOLID principle/BankService.java
@@ -0,0 +1,20 @@
+public class BankService {
+
+
+    private String username;
+    private String password;
+    long amount;
+    String accountNo;
+
+    public void  login(int username, int password)
+     {
+         // authenticate user
+     }
+     public long deposit(long amount,String accountNo){
+
+         //deposit amount
+     }
+     public long withDraw(long amount, int accountNo){
+         //withdraw amount
+     }
+}
