24a3e16f14fec4ee7aa36bc15740ea56d65c610f
Rasel Mahmud <103592581+alfred-rasel@users.noreply.github.com>
2023-08-08 23:07:28 +0600

    Create Gmail.java

---
+++ A -Senario-That-Follow -All-SOLID-Principle/Notification-Process/Gmail.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package onlineshopping;
+
+/**
+ *
+ * @author Rasel
+ */
+public class Gmail implements EmailNotifier {
+
+    @Override
+    public void sendEmail() {
+        // TODO Auto-generated method stub
+        System.out.println("Gmail email sent.");
+    }
+
+}
