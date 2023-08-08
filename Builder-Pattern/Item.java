1f3da5100f034b4e1cec6ebc324701304261404c
Rasel Mahmud <103592581+alfred-rasel@users.noreply.github.com>
2023-08-08 20:07:19 +0600

    Builder Design Pattern's Code

---
+++ Builder-Pattern/Item.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package builderpattern;
+
+/**
+ *
+ * @author Rasel
+ */
+public interface Item {
+   public String name();
+   public Packing packing();
+   public float price();	
+}
