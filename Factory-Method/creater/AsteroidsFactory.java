aa8e6f1f82fb72ae3fbdeaddbade53bc4b206892
Rasel Mahmud <103592581+alfred-rasel@users.noreply.github.com>
2023-08-08 23:48:43 +0600

    Create  AsteroidsFactory.java

---
+++ Factory-Method/creater/AsteroidsFactory.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package factory;
+
+/**
+ *
+ * @author Rasel
+ */
+public abstract class AsteroidsFactory {
+    public abstract Asteroids createAsteroids(int score);
+}
