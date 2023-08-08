76c72705d5d97f1740c759188097f993d42f0c33
Rasel Mahmud <103592581+alfred-rasel@users.noreply.github.com>
2023-08-08 23:44:33 +0600

    Create Ice_Asteroids.java

---
+++ Factory-Method/concrete_product/Ice_Asteroids.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package factory;
+
+/**
+ *
+ * @author Rasel
+ */
+public class Ice_Asteroids implements Asteroids {
+    @Override
+    public void show() {
+        System.out.println("Ice_Asteroids popped up");
+    }
+}
